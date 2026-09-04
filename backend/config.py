da88f7f995bb14cb6d2e6790cdb6a108cc7c340a
Sergio Rodriguez <serralerolo@live.com>
2026-09-03 20:07:22 -0500

    Actualiza la configuración del backend

--- backend/config.py
+++ backend/config.py
@@ -2,5 +2,5 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'clave_secreta_sies_yullita'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'mysql+pymysql://root:root1234@localhost/yullita_creaciones_db'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'mysql+pymysql://root:Sergio3650270.@localhost/yullita_creaciones_db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
